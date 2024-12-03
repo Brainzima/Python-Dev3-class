faf326e124eff449974bc9fe539992ee52f95408
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2024-12-03 15:39:27 +0530

    Create Classes.py

---
+++ Classes.py
@@ -0,0 +1,67 @@
+# print("Hello World")
+
+# name="Katihar"
+# Name="Purnea"
+# NAMe="Arariya"
+# NAME="Kishanganj"
+#
+# print(name)
+# print(NAME)
+# print(Name)
+# print(NAMe)
+#
+# num1=100
+# num2=200
+#
+# print(num1+num2)
+#
+# father_name="Papa"
+# print(father_name)
+
+
+# name="ABC Ji"
+# age="32"
+# mobile=87798798798
+# print(name)
+# print(age)
+# print(mobile)
+
+# name,age,mobile="ABC Ji",34,9879797979
+# print(name, age, mobile)
+
+
+# x=y=z="Brainzima"
+# print("value of x",x)
+# print("value of y",y)
+# print("value of z",z)
+
+# num1=345
+# num2=4565
+# add=num1+num2
+# print(add)
+#
+# print("Number1 is",num1,"Number2 is",num2,"Addition of both numbers is",add)
+
+# a="Hello"
+# b="brainzima"
+# print(a+b)
+#
+# c="55"
+# d="45"
+# print(c+d)
+# 5545
+
+num1=79
+num2=3
+print("Division",num1/num2)
+print("Modulus",num1%num2)
+
+print("Multiply",num1*num2)
+print("Num1 to the power of num2",num1**num2)
+
+print(num1//num2)
+
+
+
+
+
