4ef7e955fddeef6dbadc5d5dbb9f7a9b7bd76212
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2024-12-04 16:14:23 +0530

    Create Classes-14-12.py

---
+++ Classes-14-12.py
@@ -0,0 +1,89 @@
+# print("Hello World")
+
+# name="Katihar"
+# Name="Purnea"
+# NAMe="Arariya"
+# NAME="Kishanganj"
+#
+# print(name)
+# print(NAME)
+# print(Name)
+# print(NAMe)
+#
+# num1=100
+# num2=200
+#
+# print(num1+num2)
+#
+# father_name="Papa"
+# print(father_name)
+
+
+# name="ABC Ji"
+# age="32"
+# mobile=87798798798
+# print(name)
+# print(age)
+# print(mobile)
+
+# name,age,mobile="ABC Ji",34,9879797979
+# print(name, age, mobile)
+
+
+# x=y=z="Brainzima"
+# print("value of x",x)
+# print("value of y",y)
+# print("value of z",z)
+
+# num1=345
+# num2=4565
+# add=num1+num2
+# print(add)
+#
+# print("Number1 is",num1,"Number2 is",num2,"Addition of both numbers is",add)
+
+# a="Hello"
+# b="brainzima"
+# print(a+b)
+#
+# c="55"
+# d="45"
+# print(c+d)
+# 5545
+
+# num1=79
+# num2=3
+# print("Division",num1/num2)
+# print("Modulus",num1%num2)
+#
+# print("Multiply",num1*num2)
+# print("Num1 to the power of num2",num1**num2)
+#
+# print(num1//num2)
+
+# num=100
+# print(num)
+
+# num+=200
+# num = num + 200
+# print(num)
+
+# num-=200
+# num = num - 200
+# print(num)
+
+# num*=200
+# num = num * 200
+# print(num)
+
+# name=input()
+# print(name)
+
+name=input("Enter your name: ")
+# print("Hi, good evening",name)
+
+print("Hi",name,"You're invited to a New Year's Eve celebration on December 31st. Join us for a night of fun, food, drinks, games, and music. Please let us know if you can make it by 31st. We can't wait to celebrate the New Year with you!")
+
+Task:
+Ask user for his
+    name, fathername, address, mobile, relationship_status
