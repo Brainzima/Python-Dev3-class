af3d702dd30a7311017b6b3cb026bab621ea6df9
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-01-13 16:01:49 +0530

    add update keys values etc...

---
+++ dictDatatype-2.py
@@ -0,0 +1,39 @@
+info = {
+    'userid':'1001',
+    'password':'pass@123',
+    'name':'Rajik',
+    'age':525,
+    'mobile': 7979797997,
+    'email':'rajik@gmail.com',
+    'course':'Python'
+}
+
+# get method to access
+# print(info.get('name'))
+
+# get all the key names of dictionary
+# print(info.keys())
+
+# get all the values of dictionary
+# print(info.values())
+
+# get all the items of dictionary as tuple in list
+# print(info.items())
+
+# update or change the value of any key of dictionary
+# method 1
+# info['name'] = 'Ajit'
+# print(info)
+
+# method 2
+# info.update({'name':'Shaktimaan'})
+# print(info)
+
+# Add or insert key and value in the dictionary
+# method 1
+# print(info)
+# info['fee'] = 6500
+# print(info)
+#
+# info.update({'superhero':'Shaktimaan'})
+# print(info)
