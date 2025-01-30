4857435c0f35c8b04d315dcc441f48085526b6a6
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-01-30 16:06:17 +0530

    Functions in python

---
+++ functions.py
@@ -0,0 +1,23 @@
+# intro to function
+
+# def rajik():
+#     print('Hello Aliens')
+#
+# rajik()
+
+
+# 1
+# def sum(num1,num2):
+#     print(num1+num2)
+#
+# sum(50,500)
+# sum(54459,56464)
+
+# 2
+def sum(num1,num2):
+    print(num1+num2)
+
+num1 = int(input("Enter Number1:"))
+num2 = int(input("Enter Number2:"))
+sum(num1,num2)
+# sum(int(input("Enter Number1:")),int(input("Enter Number1:")))
