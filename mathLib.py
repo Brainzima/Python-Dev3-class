f6cd34dcb28e38a05d92b1823d6bfa40e732c0dd
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-02-04 16:04:02 +0530

    math module

---
+++ mathLib.py
@@ -0,0 +1,34 @@
+import math
+
+# square root
+# num = 455
+# print(math.sqrt(num))
+
+# pie value
+# print(math.pi)
+
+# ceil method
+# num = 1.9
+# print(math.ceil(num))
+# floor method
+# print(math.floor(num))
+
+# num = 546
+# num2 = 854
+# num3 = 845
+# print(max(num, num2, num3))
+# print(min(num, num2, num3))
+
+# absolute
+# num = -545
+# print(num)
+# print(abs(num))
+
+# power
+# num = 846
+# power = 2
+# print(pow(num,power))
+
+num = int(input("Enter the Number:"))
+power = int(input("Enter the Power:"))
+print(pow(num,power))
