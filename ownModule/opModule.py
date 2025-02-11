7cdc19e60a8894305f8c72b7608d0472941d5030
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-02-11 16:27:04 +0530

    own modules

---
+++ ownModule/opModule.py
@@ -0,0 +1,8 @@
+def sum(num1,num2):
+    return num1+num2
+def sub(num1,num2):
+    return num1-num2
+def mul(num1,num2):
+    return num1*num2
+def div(num1,num2):
+    return num1/num2
---
+++ ownModule/testModule.py
@@ -0,0 +1,16 @@
+print('We will test the module here.')
+
+# sum module test ex
+# import sumModule
+# print(sumModule.sum(100,200))
+
+# direct module import
+# import opModule
+# print(opModule.mul(10,5))
+
+# module import as renamed
+# import opModule as om
+# print(om.div(100,50))
+
+from opModule import sub
+print(sub(100,50))
