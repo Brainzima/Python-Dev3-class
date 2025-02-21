ddf9fae7a136db39ba1155e0139e10f9f2e3b4f8
M Rahman <51116745+rahman4ktr@users.noreply.github.com>
2025-02-21 16:00:37 +0530

    Opps started

---
+++ Oops/carClass.py
@@ -0,0 +1,21 @@
+class Car:
+    def __init__(self,brand,model,price):
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+
+# we will make many cars
+car1 = Car('Ford','F201',2504504)
+car1 = Car('Ford','F201',2504504)
+car1 = Car('Ford','F201',2504504)
+car1 = Car('Ford','F201',2504504)
+car1 = Car('Ford','F201',2504504)
+car1 = Car('Ford','F201',2504504)
+
+
+
+
+
+# know what you want to know
+print(car1.brand)
---
+++ Oops/intro.py
@@ -0,0 +1,8 @@
+# OOPs = Object Oriented Programming
+
+class greet:
+    name = 'Rakesh Raushan'
+
+alloo = greet()
+print(alloo)
+print(alloo.name)
